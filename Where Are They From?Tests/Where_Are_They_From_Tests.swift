afee721126bc4e0ee1772402fbf0f60b4c7987fe
RyanWhitell <robotteahouse@hotmail.com>
2015-09-22 16:22:41 -0600

    Initial Commit

---
+++ Where Are They From?Tests/Where_Are_They_From_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Where_Are_They_From_Tests.swift
+//  Where Are They From?Tests
+//
+//  Created by Ryan Whitell on 2015-09-22.
+//
+//
+
+import UIKit
+import XCTest
+
+class Where_Are_They_From_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
